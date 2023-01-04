f11efe4e3159a0b6d9d845f40677735b01cb6887
AmitThakur045 <at7129652@gmail.com>
2023-01-04 11:09:17 +0530

    added new project section

--- src/components/Project/SingleProject.js
+++ src/components/Project/SingleProject.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { data } from "./Project";
+import { useNavigate, useParams } from "react-router-dom";
+import { data } from "./data";
+import { MdArrowForwardIos, MdArrowBackIosNew } from "react-icons/md";
 
 const SingleProject = () => {
   const { slug } = useParams();
   const [value, setValue] = useState(null);
   const [id, setId] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     data.map((project) => {
       if (project.name.toLowerCase() === slug) {
         setValue(project);
+        setId(+project.id);
       }
       return 0;
     });
@@ -21,71 +24,101 @@ const SingleProject = () => {
   }
 
   return (
-    <div className="max-w-7xl py-24 px-52 space-y-4 text-[#cacaca]">
-      <h2 className="uppercase text-4xl font-bold tracking-widest  ">
-        {value.name}
-      </h2>
-
-      <p className="  text-[1.2rem] lowercase">
-        A cryptocurrency website build in React to check crypto news and their
-        prices
-      </p>
-
-      <div className="flex flex-row justify-start items-center space-x-4 w-full">
-        <a
-          href={value.github}
-          target="_blank"
-          rel="noreferrer"
-          className="uppercase w-fit text-gray-300 text-base tracking-widest p-2 px-3 bg-[#009688] font-bold rounded-sm border-b-4 hover:border-b-2 hover:bg-opacity-70 border-[#0b5951] cursor-pointer"
-        >
-          Source Code
-        </a>
-
-        {value.live.length !== 0 && (
-          <a
-            href={value.live}
-            target="_blank"
-            rel="noreferrer"
-            className="uppercase w-fit text-gray-300 text-base tracking-widest p-2 px-3 bg-[#cc181e] font-bold rounded-sm border-b-4 hover:border-b-2 hover:bg-opacity-70 border-[#660d10] cursor-pointer"
+    <div className="py-24 px-52 relative text-[#cacaca]">
+      <div className="fixed w-[5rem] h-full left-10 top-80 flex flex-row justify-center">
+        {id !== 0 && (
+          <span
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(`/project/${data[id - 1].name.toLowerCase()}`);
+            }}
+            className="p-3 rounded-full h-fit hover:bg-[#454e5a] hover:bg-opacity-20 hover:cursor-pointer"
           >
-            Live
-          </a>
+            <MdArrowBackIosNew size={50} />
+          </span>
+        )}
+      </div>
+      <div className="fixed w-[5rem] h-full right-10 top-80 flex flex-row justify-center">
+        {id !== 5 && (
+          <span
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(`/project/${data[id + 1].name.toLowerCase()}`);
+            }}
+            className="p-3 rounded-full h-fit hover:bg-[#454e5a] hover:bg-opacity-20 hover:cursor-pointer"
+          >
+            <MdArrowForwardIos size={50} />
+          </span>
         )}
       </div>
 
-      <div className="my-4 py-5">
-        <div className="bg-[#dddddd] text-[#636363] rounded-tl-md rounded-tr-md w-full text-center font-bold p-1 z-20 tracking-[6px]">
+      <div className="flex flex-col space-y-4 justify-center items-center w-full">
+        <h2 className="uppercase text-4xl font-bold tracking-widest text-left w-full">
           {value.name}
-        </div>
-        <img
-          src={value.image}
-          className="w-full aspect-auto rounded-bl-md rounded-br-md"
-          alt=""
-        />
-      </div>
-
-      <div className="my-4 py-5">
-        <h2 className="uppercase text-4xl mt-3 font-bold tracking-widest  ">
-          About the project
         </h2>
-        <div className="my-3 w-[40vw] h-[1px] bg-[#dddddd]"></div>
-        <p className="  text-xl">{value.description}</p>
-      </div>
 
-      <div>
-        <h2 className="uppercase text-4xl mt-3 font-bold tracking-widest  ">
-          Technical Sheet
-        </h2>
-        <p className="  text-xl my-2">
-          Code technologies I got involved with while working on this project
+        <p className="w-full text-left text-[1.2rem]">
+          {value.smallDesc}
         </p>
-        <div className="my-3 w-[40vw] h-[1px] bg-[#dddddd]"></div>
 
-        <ul className="text-xl list-disc pl-5">
-          {value.tech.map((curr) => (
-            <li>{curr}</li>
+        <div className="flex flex-row justify-start items-center space-x-4 w-full">
+          <a
+            href={value.github}
+            target="_blank"
+            rel="noreferrer"
+            className="uppercase w-fit text-gray-300 text-base tracking-widest p-2 px-3 bg-[#009688] font-bold rounded-sm border-b-4 hover:border-b-2 hover:bg-opacity-70 border-[#0b5951] cursor-pointer"
+          >
+            Source Code
+          </a>
+
+          {value.live.length !== 0 && (
+            <a
+              href={value.live}
+              target="_blank"
+              rel="noreferrer"
+              className="uppercase w-fit text-gray-300 text-base tracking-widest p-2 px-3 bg-[#cc181e] font-bold rounded-sm border-b-4 hover:border-b-2 hover:bg-opacity-70 border-[#660d10] cursor-pointer"
+            >
+              Live
+            </a>
+          )}
+        </div>
+
+        <div className="my-4 py-5">
+          <div className="bg-[#dddddd] text-[#636363] rounded-tl-md rounded-tr-md w-full text-center font-bold p-1 z-20 tracking-[6px]">
+            {value.name}
+          </div>
+          <img
+            src={value.image}
+            className="w-full aspect-auto rounded-bl-md rounded-br-md"
+            alt=""
+          />
+        </div>
+
+        <div className="my-4 py-5">
+          <h2 className="uppercase text-4xl mt-3 font-bold tracking-widest  ">
+            About the project
+          </h2>
+          <div className="my-3 w-[40vw] h-[1px] bg-[#dddddd]"></div>
+          {value.description.map((project) => (
+            <p className="text-[1.1rem] text-opacity-70">{project}</p>
           ))}
-        </ul>
+        </div>
+
+        <div className="w-full">
+          <h2 className="uppercase text-4xl mt-3 font-bold tracking-widest w-full text-left">
+            Technical Sheet
+          </h2>
+          <p className="  text-xl my-2">
+            Code technologies I got involved with while working on this project
+          </p>
+          <div className="my-3 w-[40vw] h-[1px] bg-[#dddddd]"></div>
+
+          <ul className="text-xl list-disc pl-5">
+            {value.tech.map((curr) => (
+              <li>{curr}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Project/data.js
@@ -0,0 +1,107 @@
+export const data = [
+  {
+    id: 0,
+    name: "CodeBuddies",
+    description: [
+      "Codebuddies is a MERN web app that provides a virtual environment for multiple users to code together and chat in real-time. It uses web sockets, a technology that enables real-time communication between a server and clients, to allow multiple users to code and chat simultaneously. Codebuddies has an integrated development environment (IDE) and code editor, allowing users to write and collaborate on code projects. The app has a user-friendly interface that makes it easy for users to join and participate in coding sessions, and the real-time chat feature allows users to discuss and troubleshoot problems as they arise. Whether you're a beginner looking to learn to code with others or an experienced developer looking for a collaborative workspace, Codebuddies is the perfect platform for you.",
+    ],
+    smallDesc:
+      "Codebuddies is a MERN web app that provides a virtual environment for real-time coding and chatting with web sockets.",
+    image: "/codebuddies.JPG",
+    live: "https://codebuddies.netlify.app/",
+    github: "https://github.com/AmitThakur045/Code-Buddy",
+    tech: [
+      "React",
+      "RecoilJs",
+      "Tailwind",
+      "Socket",
+      "Node",
+      "MongoDB",
+      "Express",
+    ],
+  },
+  {
+    id: 1,
+    name: "Bessalani-lms",
+    description: [
+      "Bessalani LMS is a virtual education platform that aims to provide a traditional learning experience in a digital environment. It is built using the MERN stack, a combination of technologies including MongoDB, Express, React, and Node.js.",
+      "The platform has two panels: a student panel and an admin panel. In the student panel, students can change their batch, view their performance, watch recorded lectures, track their classes with a calendar, and submit assignments. The admin panel, on the other hand, allows the administrator to create batches, courses, assignments, upload recorded lectures, add students, and mark attendance.",
+      "Bessalani LMS provides a convenient and flexible way for students to access and manage their educational materials and progress, and gives admins the tools to effectively manage and track their students' progress.",
+    ],
+    smallDesc:
+      "Bessalani LMS is a virtual education platform, featuring a student panel for accessing and managing educational materials and a admin panel for managing and tracking student progress.",
+    image:
+      "https://raw.githubusercontent.com/AmitThakur045/LMS/main/Docs-Images/Student_Course.JPG",
+    live: "https://bessalani-lms.netlify.app/",
+    github: "https://github.com/AmitThakur045/Project1",
+    tech: ["React", "Redux", "Tailwind", "Node", "MongoDB", "Express", "AWS"],
+  },
+  {
+    id: 2,
+    name: "Podcast",
+    description: [
+      "Podcast is an audio platform that allows multiple users to voice chat with each other in real-time. It uses WebRTC, a technology that enables peer-to-peer communication directly between web browsers, and Socket.io, a library for real-time communication on the web, to enable real-time voice conversations between users. Podcast is built using the MERN stack, which stands for MongoDB, Express, React, and Node.js. The app has a user-friendly interface that allows users to easily create and join voice chats, and the audio quality is clear and lag-free thanks to the use of WebRTC. Whether you want to catch up with friends, hold a business meeting, or just chat with like-minded people, Podcast is the perfect tool for you.",
+    ],
+    smallDesc:
+      "Podcast is a real-time audio chat platform built with the MERN stack and powered by WebRTC and Socket.io.",
+    image: "/podcast.JPG",
+    live: "",
+    github: "https://github.com/AmitThakur045/podcast",
+    tech: [
+      "React",
+      "Redux-Toolkit",
+      "Socket",
+      "WebRTC",
+      "Node",
+      "MongoDB",
+      "Express",
+    ],
+  },
+  {
+    id: 3,
+    name: "Parivesh",
+    description: [
+      "Parivesh is a healthcare waste management platform that helps to streamline the process of managing and disposing of healthcare waste (HCW). It includes three stages: production, training and awareness, and usage/disposal. Manufacturers can use the platform to mark their products with QR codes that classify the waste by type and provide information on the correct disposal method. Parivesh also provides training and awareness modules to small clinics and hospitals to help them manage HCW during usage, and uses a machine learning model to classify waste based on its visual appearance and provide information on the correct disposal method. This helps to ensure that HCW is properly identified, contained, and disposed of, protecting public health and the environment.",
+    ],
+    smallDesc:
+      "Parivesh is a three tier solution for the problem of healthcare waste management working on stages from waste production to waste disposal.",
+    image: "/parivesh.JPG",
+    live: "https://parivesh.vercel.app/",
+    github: "https://github.com/gaganvats-05/parivesh",
+    tech: [
+      "React",
+      "Tailwind",
+      "Auth0",
+      "ML Model",
+      "Node",
+      "MongoDB",
+      "Express",
+    ],
+  },
+  {
+    id: 4,
+    name: "Shareme",
+    description: [
+      "The website is a platform for sharing photos. Users can authenticate themselves using the Google API. Once logged in, they can save, like, and upload images. The website is built using React, a popular JavaScript library for building user interfaces. The platform has a user-friendly interface that makes it easy for users to navigate and interact with the site. Whether you want to share photos with friends and family, or connect with like-minded people and discover new images, this website is the perfect place to do it.",
+    ],
+    smallDesc:
+      "The Shareme website built in React allows users to share and interact with photos, authenticating with the Google API.",
+    image: "/shareme.JPG",
+    live: "https://shareme-amitthakur.netlify.app/",
+    github: "https://github.com/AmitThakur045/ShareMe-App",
+    tech: ["React", "Sanity", "Tailwind", "Redux-Toolkit", "Google-api"],
+  },
+  {
+    id: 5,
+    name: "Crptoverse",
+    description: [
+      "Cryptoverse is a platform for checking the latest cryptocurrency news and prices. It uses the RapidAPI and ReactJS to fetch data on different cryptocurrencies, and displays this information to users in an easy-to-use interface. The website also utilizes the Bing search API to provide real-time data on cryptocurrencies, helping users stay up to date on the latest developments in the crypto market. The platform is built using React, a popular JavaScript library for building user interfaces, and has a user-friendly interface that makes it easy for users to navigate and interact with the site. Whether you're an experienced cryptocurrency trader or just starting out, this website is a valuable resource for staying informed about the crypto market.",
+    ],
+    smallDesc:
+      "A cryptocurrency website build in React to check crypto news and their prices.",
+    image: "/cryptoverse.JPG",
+    live: "https://cryptoverse-amitthakur045.netlify.app/",
+    github: "https://github.com/AmitThakur045/CryptoVerse-web-app",
+    tech: ["React", "Tailwind", "RapidApi"],
+  },
+];
